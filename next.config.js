ac6f0adf0b73e41b0ace1c9b830790f17bda738b
Gawain Bracy II <gawainbracyii@gawains-mbp.lan>
2022-08-06 09:16:52 -0400

    exportPath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  trailingSlash: true,
   reactStrictMode: true,
   swcMinify: true,
+  exportPathMap: async function (
+  ) {
+    return {
+      '/': { page: '/' },
+    }
+  }
 }
 
 module.exports = nextConfig
