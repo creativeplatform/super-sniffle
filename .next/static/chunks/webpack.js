10a8e967a4e5f325f716ba190ec85d33c492921e
Gawain Bracy II <gawainbracyii@gawains-mbp.lan>
2022-08-06 11:47:12 -0400

    update typography

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "26266af6c7d0cc1b"; }
+/******/ 		__webpack_require__.h = function() { return "e22ac5035c60285d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
