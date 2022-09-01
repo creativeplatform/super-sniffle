a9a31d9948a318edb7975fcc79161708181270f8
Shubham Doharey <shubham.doharey@gmail.com>
2022-09-01 15:02:42 +0530

    Update Minting.js

--- components/Minting.js
+++ components/Minting.js
@@ -385,7 +385,7 @@ const editionDrop = useEditionDrop("0x7CCA079B8B8E9857fe0cB1CDA433Fda2F703f9CE")
                             <video
                               autoplay
                               loop
-                              controls
+                              
                               className="object-scale-down max-w-fit max-h-[21rem]"
                               alt="UUC Animation"
                             >
