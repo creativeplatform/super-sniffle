dc4d94464a7df803af66b597b4839e38b8d2540d
Gawain Bracy II <gawainbracyii@gawains-mbp.lan>
2022-09-14 14:53:55 -0400

    update provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,20 @@
 import '../styles/globals.css'
-import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react"
-
-const activeChainId = ChainId.Mumbai;
+import { ChainId, IpfsStorage, ThirdwebProvider } from "@thirdweb-dev/react"
 
 function MyApp({ Component, pageProps }) {
   return(
-    <ThirdwebProvider desiredChainId={activeChainId}>
+    <ThirdwebProvider 
+    desiredChainId={ChainId.Polygon}
+    chainRpc={{ [ChainId.Polygon]: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_POLYGON_API}` }}
+    dAppMeta={{
+      name: "Urban Uprise Crew",
+      description: "Join the Revolution, Free Your Creative Soul",
+      isDarkMode: true,
+      logoUrl: "https://urbanuprisecrew.com/wp-content/uploads/2022/02/UUClogoWeb.png",
+      url: "https://urbanuprisecrew.com",
+    }}
+    supportedChains={[ChainId.Mumbai]}
+    >
       <Component {...pageProps} />
     </ThirdwebProvider>
     
