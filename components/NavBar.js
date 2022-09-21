44b21cddcd12ed5e6b4a5ae6581f681c0bebb6f2
G2 <bracy.gawain2014@gmail.com>
2022-09-21 09:34:20 -0400

    Revert "Token Swap Integration"

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,16 +1,13 @@
 import React from "react";
 import Link from "next/link";
 import Logout from "./Logout";
-//import Swap from "./Swap";
 import { useAddress,ConnectWallet } from "@thirdweb-dev/react";
 import { motion } from "framer-motion";
-import dynamic from "next/dynamic";
 function NavBar() {
   const address = useAddress();
   const styles = {
     headerLink: `text-base font-medium text-white hover:text-KeenIcyBlue-80 mr-4`,
   };
-const Swap = dynamic( () => {return import("./Swap");},{ ssr: false });
 
   return (
     <>
@@ -18,27 +15,24 @@ const Swap = dynamic( () => {return import("./Swap");},{ ssr: false });
         <div className="w-full">
           {address ? (
             <>
-              <nav className="relative flex flex-wrap items-center justify-between py-2">
-                <div className="w-full mx-auto flex flex-wrap items-center justify-between px-5 sm:px-7">
-                  <div className="smcenter relative flex justify-between mb-2 md:my-auto w-auto lg:static lg:block lg:justify-start">
+              <nav className="relative flex flex-wrap items-center justify-between py-3">
+                <div className="w-full mx-auto flex flex-wrap items-center justify-between px-7">
+                  <div className=" relative flex justify-between w-auto lg:static lg:block lg:justify-start">
                     <img
                       src="/assets/Logo.jpg"
                       className="float-left w-11 h-11 rounded-full"
                       alt="Urban Uprise Crew"
                     />
                   </div>
-                  <ul className="flex flex-rows-3 gap-3 mx-auto sm:mr-3 " >
-                  <li className="nav-item mx-auto ">
-                  <Swap/>
-                  </li>
-                    <li className="nav-item mx-auto  ">
+
+                  <ul className="flex flex-row list-none ml-auto ">
+                    <li className="nav-item mr-3 ">
                             <ConnectWallet />
                     </li>
 
                     <AuthenticatedHeader />
                       
                   </ul>
-                  
                 </div>
               </nav>
             </>
@@ -68,7 +62,7 @@ const Swap = dynamic( () => {return import("./Swap");},{ ssr: false });
 
 const AuthenticatedHeader = () => (
   <>
-    <li className="nav-item mx-auto">
+    <li>
       <Logout />
     </li>
   </>
